ae8f719c72cd689047af030cf88edd85c85abbb3
Serhii <serhii.mask@gmail.com>
2024-01-06 18:35:03 +0200

    feat: change all files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,8 @@
 package org.example;
 
-import org.example.calculator.SumCalculator;
-
-import java.util.logging.Logger;
-
 public class Main {
 
     public static void main(String[] args) {
-        SumCalculator calculator = new SumCalculator();
-        Logger logger = Logger.getLogger(Main.class.getName());
-
-        logger.info(String.valueOf(calculator.sum(3)));
+        System.out.println("Hello world!");
     }
 }
